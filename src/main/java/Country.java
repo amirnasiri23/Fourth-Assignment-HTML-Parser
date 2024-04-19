b3981b3a1fa1b8b25879a1625fdb9255a0bbc5e8
AmirAli <amirnasiri1383710@gmail.com>
2024-04-19 12:59:35 +0330

    Country Class complete

--- src/main/java/Country.java
+++ src/main/java/Country.java
@@ -7,8 +7,10 @@ public class Country {
     private double area;
 
     public Country(String name, String capital, int population, double area) {
-        //TODO
-
+        this.name = name;
+        this.capital = capital;
+        this.population = population;
+        this.area = area;
     }
 
     public String getName() {
@@ -29,8 +31,13 @@ public class Country {
 
     @Override
     public String toString() {
-        //TODO
-        return "";
+        String countryShow = "--------------Country--------------\n";
+        countryShow += "Name : " + this.name + "\n";
+        countryShow += "Capital : " + this.capital + "\n";
+        countryShow += "Population : " + this.population + "\n";
+        countryShow += "Area : " + this.area + "\n";
+
+        return countryShow;
     }
 
     @Override
