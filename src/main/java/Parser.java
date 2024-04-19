7581c108be06e1a163e3ea59d1ea36fe700dce82
AmirAli <amirnasiri1383710@gmail.com>
2024-04-19 13:28:45 +0330

    parsing htmlFile

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -12,28 +12,33 @@ public class Parser {
     public List<Country> sortByName(){
         List<Country> sortedByName = new ArrayList<>(countries);
         // Sort countries alphabetically (least)
-        //TODO
+        sortedByName.sort(Comparator.comparing(Country::getName));
         return  sortedByName;
     }
 
     public List<Country> sortByPopulation(){
         List<Country> sortedByPopulation = new ArrayList<>(countries);
         // Sort countries by population (most)
-        //TODO
+        sortedByPopulation.sort(Comparator.comparing(Country::getPopulation));
+        Collections.reverse(sortedByPopulation);
         return sortedByPopulation;
     }
 
     public List<Country> sortByArea(){
         List<Country> sortedByArea = new ArrayList<>(countries);
         // Sort countries by area (most)
-        //TODO
+        sortedByArea.sort(Comparator.comparing(Country::getArea));
+        Collections.reverse(sortedByArea);
         return sortedByArea;
     }
 
-    public void setUp() throws IOException {
+    public static void setUp() throws IOException {
 
         //Parse the HTML file using Jsoup
-        //TODO
+        File htmlFile = new File("D:/usb/ap/assignment/a4/Fourth-Assignment-HTML-Parser/src/Resources/country-list.html");
+        Document document = Jsoup.parse(htmlFile, "UTF-8");
+
+        System.out.println(document.title());
 
         // Extract data from the HTML
         //TODO
@@ -42,7 +47,8 @@ public class Parser {
         //TODO
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //you can test your code here before you run the unit tests ;)
+        setUp();
     }
 }
